22cc6bf088d4d813718510c52a5984340a1a87f3
Dan <danieldawes@protonmail.com>
2022-11-22 13:53:48 +0100

    added aboput me section

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import NavigationBar from "./components/NavigationBar.vue";
 import Hero from "./components/Hero.vue"
 import ProjectsSection from "./components/ProjectsSection.vue";
+import About from "./components/About.vue";
 </script>
 
 <template>
   <NavigationBar />
   <Hero />
+  <About />
   <ProjectsSection />
 </template>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,12 @@
 <script setup></script>
 <template>
-  <h2 class="text-4xl font-display mb-8">
-    <span class="font-body text-emerald-400 text-2xl">00.</span>
-    About Me
-  </h2>
-
+  <div class="w-5/6 mx-14">
+    <h2 class="text-4xl font-display mb-8">
+      <span class="font-body text-emerald-400 text-2xl">00.</span>
+      Who am I?
+    </h2>
+    <p>My journey with code started at univeristy with the ever so <span class="line-through">not</span> friendly Assembly & C++ languages. Despite achieving a first-class degree in Electronics Engineering, my programming skills still left a lot to be desired. Since then, I have deep dived into the JavaScript ecosystem, focusing in on Vue, Nuxt & TypeScript. </p>
+    <p> I have developed a passion for design, animation, interaction, problem-solving, and with mastering the latest front-end technologies.</p>
+  </div>
+  
 </template>
